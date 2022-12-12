339147b2362bb622df53ebbebc7ca2fdf233b362
Dorcas@126 <uwimbabazidorcas65@gmail.com>
2022-12-12 17:01:08 +0000

    commits

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,21 +1,6 @@
-# Client configuration file (w/ Puppet)
-
-# Your SSH client configuration must be configured to use
-# the private key ~/.ssh/school
-# Your SSH client configuration must be configured to refuse
-# to authenticate using a password
-include stdlib
-file_line { 'NO password'
-    ensure             => present,
-    path               => '/etc/ssh/ssh_config',
-    line               => '    PasswordAuthentication no',
-    match              => '^PasswordAuthentication',
-    append_on_no_match => true,
-  }
-file_line { 'New private key holder'
-    ensure             => present,
-    path               => '/etc/ssh/ssh_config',
-    line               => '    IdentityFile ~/.ssh/school',
-    match              => '^IdentityFile'
-    append_on_no_match => true,
-  }
+# Changes SSH config file
+exec { 'echo':
+  path    => 'usr/bin:/bin',
+  command => 'echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  returns => [0,1],
+}
